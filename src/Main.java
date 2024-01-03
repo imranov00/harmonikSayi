09ec62954759e64874534f317e534cc5be88b833
İmranov <semtexpert@gmail.com>
2024-01-03 15:34:02 +0300

    version 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double[] dizi = diziyeEkleme();
+        double harmonikToplam = 0;
+        for (double eleman : dizi) {
+            harmonikToplam += 1 / eleman;
+        }
+        double harmonikOrtalama = dizi.length / harmonikToplam;
+        System.out.println("Dizinin Harmonik Ortalaması " + harmonikOrtalama);
+    }
+
+    public static double[] diziyeEkleme() {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Dizinin Eleman Sayısnı Giriniz: ");
+        int n = inp.nextInt();
+
+        double[] dizi = new double[n];
+
+        System.out.println("Dizinin Elemanlarını Giriniz;");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Eleman " + ( i+1) + ": ");
+            dizi[i] = inp.nextDouble();
+            
+        }
+        return dizi;
     }
 }
